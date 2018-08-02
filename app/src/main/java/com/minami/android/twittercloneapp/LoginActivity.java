a37039efb98589f25ea957ab486229aaf9bbb6a2
Minamiciccc <june28munazo@gmail.com>
2018-08-02 14:46:02 -0700

    initial commit.

--- app/src/main/java/com/minami/android/twittercloneapp/LoginActivity.java
+++ app/src/main/java/com/minami/android/twittercloneapp/LoginActivity.java
@@ -3,11 +3,34 @@ package com.minami.android.twittercloneapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TwitterLoginButton mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // ...
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+
 }
